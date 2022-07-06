abb1967b7fd6ad536c42f88dc79b44d93f00ec26
theophile <chmeredith@gmail.com>
2022-07-06 11:06:09 -0500

    Initial commit of Armbian 5.10.y with patches

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#2 SMP PREEMPT Wed Jul 6 00:40:38 CDT 2022"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "Lightfoot"
+#define LINUX_COMPILER "aarch64-linux-gnu-gcc (GNU Toolchain for the A-profile Architecture 8.3-2019.03 (arm-rel-8.36)) 8.3.0, GNU ld (GNU Toolchain for the A-profile Architecture 8.3-2019.03 (arm-rel-8.36)) 2.32.0.20190321"
